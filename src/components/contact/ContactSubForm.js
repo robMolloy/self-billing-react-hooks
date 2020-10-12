9029946e7a4f17adf9b927d152633fad81eaa44c
robMolloy <robmolloy@hotmail.co.uk>
2020-10-12 23:26:41 +0100

    form behaviour

--- src/components/contact/ContactSubForm.js
+++ src/components/contact/ContactSubForm.js
@@ -1,28 +1,59 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+// import "yup-phone";
+
+import GridButton from "../custom/GridButton";
+import GridItem from "../custom/GridItem";
+import GridInput from "../custom/GridInput";
+
+import { contactMethods, contactTypes } from "../../contexts/options";
+import { ucFirst } from "../../user_modules/StringManipulation";
+import GridSelect from "../custom/GridSelect";
+import { v4 as uuid } from "uuid";
+
+import { parsePhoneNumberFromString } from "libphonenumber-js";
+
+const normalizePhoneNumber = (value) => {
+  const phoneNumber = parsePhoneNumberFromString(value);
+  return !phoneNumber ? value : phoneNumber.formatInternational();
+};
+
 const schema = yup.object().shape({
-  con_name: yup.string().required("Contact name is a required field"),
-  con_address: yup.string().required("Contact address is a required field"),
+  con_type: yup.string().required("Type is a required field"),
+  con_method: yup.string().required("Method is a required field"),
+  con_address: yup.string().required("Address is a required field"),
+  con_address_phone: yup
+    .string()
+    .phone()
+    .required("Phone number is a required field"),
+  con_address_email: yup
+    .string()
+    .required("Email address is a required field")
+    .email("must be an email address"),
 });
 
-const ContactSubForm = ({ contacts, ...props }) => {
-  // const { register, watch, handleSubmit, errors } = useForm({
-  const { register, watch } = useForm({
+const CustomerForm = ({ contacts, contactsState, setContactsState }) => {
+  const { register, watch, getValues, errors, trigger, reset } = useForm({
+    defaultValues: { con_type: "", con_method: "", con_address: "" },
     mode: "onChange",
     resolver: yupResolver(schema),
   });
 
   const con_type = watch("con_type");
 
-  contacts = contacts === undefined ? { [uuid()]: contactBlankRow } : contacts;
-
   return (
-    <GridContainer>
+    <>
+      <GridItem>Contacts</GridItem>
       <GridSelect
-        grid={{ xs: 6, sm: 2 }}
+        autoFocus
+        grid={{ xs: 6, sm: 3 }}
         inputRef={register}
         defaultValue=""
         name="con_type"
         label="Type"
-        required
+        error={!!errors.con_type}
       >
         <option disabled value={""}></option>
         {contactTypes.map((contactType) => (
@@ -31,13 +62,13 @@ const ContactSubForm = ({ contacts, ...props }) => {
           </option>
         ))}
       </GridSelect>
-
       <GridSelect
-        grid={{ xs: 6, sm: 2 }}
+        grid={{ xs: 6, sm: 3 }}
+        inputRef={register}
         name="con_method"
         defaultValue=""
         label="Method"
-        required
+        error={!!errors.con_method}
       >
         <option disabled value={""}></option>
         {(contactMethods[con_type] ? contactMethods[con_type] : []).map(
@@ -48,20 +79,76 @@ const ContactSubForm = ({ contacts, ...props }) => {
           )
         )}
       </GridSelect>
+      {con_type === "" && (
+        <GridInput
+          grid={{ xs: 9, sm: 4 }}
+          inputRef={register}
+          name="con_address"
+          label="Address"
+          error={!!errors.con_address}
+          helperText={errors?.con_address?.message}
+        />
+      )}
+      {con_type === "phone" && (
+        <GridInput
+          grid={{ xs: 9, sm: 4 }}
+          inputRef={register}
+          name="con_address_phone"
+          onChange={(e) => {
+            e.target.value = normalizePhoneNumber(e.target.value);
+          }}
+          label="Phone Number"
+          error={!!errors.con_address_phone}
+          helperText={errors?.con_address_phone?.message}
+        />
+      )}
+      {con_type === "email" && (
+        <GridInput
+          grid={{ xs: 9, sm: 4 }}
+          inputRef={register}
+          name="con_address_email"
+          label="Email Address"
+          error={!!errors.con_address_email}
+          helperText={errors?.con_address_email?.message}
+        />
+      )}
 
-      <GridInput
-        grid={{ xs: 9, sm: 5 }}
-        ref={register}
-        defaultValue=""
-        name="con_address"
-        label="Address"
-        required
-      />
-      <GridButton grid={{ xs: 3 }} variant="outlined">
+      <GridButton
+        grid={{ xs: 2 }}
+        variant="outlined"
+        onClick={async () => {
+          await trigger();
+          const errorKeys = Object.keys(errors);
+          const valid = errorKeys.length === 0;
+
+          if (valid) {
+            const { con_type, con_method, ...addressProps } = getValues();
+            const con_address = Object.values(addressProps).filter(Boolean);
+            let newContact = { con_type, con_method, con_address };
+            setContactsState({ ...contactsState, [uuid()]: newContact });
+
+            reset();
+          }
+
+          // const inputFocus = errorKeys[0] ? errorKeys[0] : "con_type";
+          // console.log(inputFocus);
+
+          // document.querySelector(`[name = "${inputFocus}"]`).focus();
+        }}
+      >
         +
       </GridButton>
-    </GridContainer>
+
+      {Object.values(contactsState).map((contact) => (
+        <React.Fragment key={uuid()}>
+          <GridItem xs={3}>{contact.con_type}</GridItem>
+          <GridItem xs={3}>{contact.con_method}</GridItem>
+          <GridItem xs={4}>{contact.con_address}</GridItem>
+          <GridItem xs={2}></GridItem>
+        </React.Fragment>
+      ))}
+    </>
   );
 };
 
-export default ContactSubForm;
+export default CustomerForm;
--- src/components/customer/CustomerForm.js
+++ src/components/customer/CustomerForm.js
@@ -2,40 +2,19 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import "yup-phone";
 
 import GridButton from "../custom/GridButton";
-import GridItem from "../custom/GridItem";
 import MainContainer from "../custom/MainContainer";
 import Form from "../custom/Form";
 import GridInput from "../custom/GridInput";
 import GridContainer from "../custom/GridContainer";
+import ContactSubForm from "../contact/ContactSubForm";
 
-import { contactMethods, contactTypes } from "../../contexts/options";
-import { ucFirst } from "../../user_modules/StringManipulation";
-import GridSelect from "../custom/GridSelect";
 import { v4 as uuid } from "uuid";
 
 import useContactReducer from "../../custom_hooks/useContactReducer";
 import useCustomerReducer from "../../custom_hooks/useCustomerReducer";
-
-// import { CustomerContext } from "../../contexts/CustomerContext";
-// import { ContactContext } from "../../contexts/ContactContext";
-// import React, { useState, useContext } from "react";
-// import { ContactsOnCustomerContext } from "../../contexts/ContactsOnCustomerContext";
-
-// import ContactSubForm from "../contact/ContactSubForm";
-
-// import React, { useState } from "react";
-// import React from "react";
-// import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import * as yup from "yup";
-// import GridInput from "../custom/GridInput";
-// import GridButton from "../custom/GridButton";
-// import GridContainer from "../custom/GridContainer";
-
-// import React from "react";
-// import { contactBlankRow } from "../../object_info/blankRows";
 
 const schema = yup.object().shape({
   cus_first_name: yup
@@ -54,29 +33,14 @@ const CustomerForm = ({ contacts, ...props }) => {
   const { addCustomer } = useCustomerReducer();
   const { addContacts } = useContactReducer();
 
-  const {
-    register,
-    handleSubmit,
-    errors,
-    watch,
-    setValue,
-    getValues,
-  } = useForm({
+  // setValue,
+  // getValues,
+
+  const { register, handleSubmit, errors } = useForm({
     defaultValues: { cus_first_name: "", cus_last_name: "" },
     mode: "onChange",
     resolver: yupResolver(schema),
   });
-
-  const cus_first_name = watch("cus_first_name");
-  const cus_last_name = watch("cus_last_name");
-
-  const con_type = watch("con_type");
-
-  let showContactForm =
-    !!cus_first_name &&
-    !!cus_last_name &&
-    !errors.cus_first_name &&
-    !errors.cus_last_name;
 
   let [contactsState, setContactsState] = useState(contacts);
 
@@ -89,105 +53,34 @@ const CustomerForm = ({ contacts, ...props }) => {
   };
 
   return (
-    <MainContainer maxWidth="sm">
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <GridContainer>
-          <GridInput
-            grid={{ xs: 12 }}
-            ref={register}
-            name="cus_first_name"
-            label="First Name"
-            autoFocus
-            error={!!errors.cus_first_name}
-            helperText={errors?.cus_first_name?.message}
-          />
-          <GridInput
-            ref={register}
-            name="cus_last_name"
-            label="Last Name"
-            error={!!errors.cus_last_name}
-            helperText={errors?.cus_last_name?.message}
-          />
-          {showContactForm && (
-            <>
-              <GridItem>Contacts</GridItem>
+    <>
+      <MainContainer maxWidth="sm">
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <GridContainer>
+            <GridInput
+              grid={{ xs: 12 }}
+              inputRef={register}
+              name="cus_first_name"
+              label="First Name"
+              autoFocus
+              error={!!errors.cus_first_name}
+              helperText={errors?.cus_first_name?.message}
+            />
+            <GridInput
+              inputRef={register}
+              name="cus_last_name"
+              label="Last Name"
+              error={!!errors.cus_last_name}
+              helperText={errors?.cus_last_name?.message}
+            />
 
-              <GridSelect
-                grid={{ xs: 6, sm: 2 }}
-                inputRef={register}
-                defaultValue=""
-                name="con_type"
-                label="Type"
-                required
-              >
-                <option disabled value={""}></option>
-                {contactTypes.map((contactType) => (
-                  <option key={contactType} value={contactType}>
-                    {ucFirst(contactType)}
-                  </option>
-                ))}
-              </GridSelect>
+            <ContactSubForm {...{ contactsState, setContactsState }} />
 
-              <GridSelect
-                grid={{ xs: 6, sm: 2 }}
-                inputRef={register}
-                name="con_method"
-                defaultValue=""
-                label="Method"
-                required
-              >
-                <option disabled value={""}></option>
-                {(contactMethods[con_type] ? contactMethods[con_type] : []).map(
-                  (contactMethod) => (
-                    <option key={contactMethod} value={contactMethod}>
-                      {ucFirst(contactMethod)}
-                    </option>
-                  )
-                )}
-              </GridSelect>
-
-              <GridInput
-                grid={{ xs: 9, sm: 5 }}
-                ref={register}
-                defaultValue=""
-                name="con_address"
-                label="Address"
-                required
-              />
-              <GridButton
-                grid={{ xs: 3 }}
-                variant="outlined"
-                onClick={() => {
-                  let data = getValues();
-
-                  let newContact = {
-                    con_type: data.con_type,
-                    con_method: data.con_method,
-                    con_address: data.con_address,
-                  };
-
-                  setContactsState({ ...contactsState, [uuid()]: newContact });
-
-                  setValue("con_type", "");
-                  setValue("con_method", "");
-                  setValue("con_address", "");
-                }}
-              >
-                +
-              </GridButton>
-            </>
-          )}
-          {Object.values(contactsState).map((contact) => (
-            <React.Fragment key={uuid()}>
-              <GridItem xs={4}>{contact.con_type}</GridItem>
-              <GridItem xs={4}>{contact.con_method}</GridItem>
-              <GridItem xs={4}>{contact.con_address}</GridItem>
-            </React.Fragment>
-          ))}
-          <GridButton type="submit">Submit</GridButton>
-        </GridContainer>
-      </Form>
-    </MainContainer>
+            <GridButton type="submit">Submit</GridButton>
+          </GridContainer>
+        </Form>
+      </MainContainer>
+    </>
   );
 };
 
